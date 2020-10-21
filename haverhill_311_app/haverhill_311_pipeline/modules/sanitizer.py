fba80645fd88f7348d896d613f012b3d8f3ff513
TejOP <tejsome@gmail.com>
2020-10-21 12:04:33 -0400

    Removed personal info and irrelevant fields

--- haverhill_311_app/haverhill_311_pipeline/modules/sanitizer.py
+++ haverhill_311_app/haverhill_311_pipeline/modules/sanitizer.py
@@ -1,3 +1,15 @@
 """The sanitizer module cleans and processes 311 data retreived from QAlert
     in preperation for storage in db.
 """
+
+def sanitize(data):
+    
+    """ Clean is a list of keys that need to be deleted """
+
+    clean = ["createDateUnix", "id", "priorityToDisplay", "status", "lastAction", "lastActionUnix", "hasLinks", "streetNum", "streetName", "crossName", "cityName", "name", "email" ]
+    for dictionary in data: 
+        for j in clean: 
+            dictionary.pop(j)
+    return data
+
+
